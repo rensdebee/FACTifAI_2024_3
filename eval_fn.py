5a1d7e6ae6954b9c472c1c755542107522f1a2fe
Rensdebee <R.35rens@gmail.com>
2024-01-16 16:39:12 +0100

    Fix formatting

--- eval_fn.py
+++ eval_fn.py
@@ -245,11 +245,11 @@ def evaluation_function(
     return metric_vals
 
 
-# if __name__ == "__main__":
-#     evaluation_function(
-#         "bcos",
-#         "FT\VOC2007/bcos_finetunedobjlocpareto_attrBCos_loclossEnergy_origmodel_checkpoint_f1_best.pt_resnet50_lr0.0001_sll0.001_layerInput\model_checkpoint_f1_best.pt",
-#         "Energy",
-#         "Input",
-#         "BCos",
-#     )
+if __name__ == "__main__":
+    evaluation_function(
+        "bcos",
+        "FT\VOC2007/bcos_finetunedobjlocpareto_attrBCos_loclossEnergy_origmodel_checkpoint_f1_best.pt_resnet50_lr0.0001_sll0.001_layerInput\model_checkpoint_f1_best.pt",
+        "Energy",
+        "Input",
+        "BCos",
+    )
